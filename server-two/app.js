970f6bad171bd305a4a4d9251007364f5d180175
maris <mari.seppanen4@metropolia.fi>
2021-03-16 16:38:06 +0200

    First commit

---
+++ server-two/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+app.use(express.static("public"));
+
+/*app.get("/", (req, res) => {
+  res.send("Hello World!");
+});*/
+
+app.get("/", (req, res) => {
+  const uiContent = {
+    title: "Example",
+    heading: "Click on the cat",
+  };
+  res.render("index", uiContent);
+});
+
+app.get("/catinfo", (req, res) => {
+  const cat = {
+    name: "Frank",
+    age: 6,
+    weight: 5,
+  };
+  res.json(cat);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
